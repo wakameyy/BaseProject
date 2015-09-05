ab0c5585e41d6478420324fb13cc615633a1500f
wakameyy <wakameyy@gmail.com>
2015-09-05 22:10:15 +0900

    コマンドラインのからの入力の引数受け取り処理の修正

--- Assets/Editor/CommandLineBridge.cs
+++ Assets/Editor/CommandLineBridge.cs
@@ -1,55 +1,49 @@
 ﻿/**
  * コマンドラインから起動したの関数の引数を受け取るクラス。
- *
+ * 
  * @author wakameyy
  * @date   2015/09/05
  */
 using System.Collections.Generic;
 
-public static class CommandLineBridge {
+namespace WakLib {
+    ///<summary>
+    /// string[] args = WakLib.CommnadLineBridge.GetMethodParametors();</br>
+    /// のように呼び出せば取得できる。
+    ///</summary>
+    public static class CommandLineBridge {
 
-    public static string[] GetMethodParametors()
-    {
-        const string EXECUTE_METHOD = "-executeMethod";
-
-        string[] cmds = System.Environment.GetCommandLineArgs();
-        List<string> args = new List<string>();
-        bool isExecuteMethod = false;
-        foreach( string str in cmds )
+        public static string[] GetMethodParametors()
         {
-            if( isExecuteMethod )
+            const string EXECUTE_METHOD = "-executeMethod";
+
+            string[] cmds = System.Environment.GetCommandLineArgs();
+            List<string> args = new List<string>();
+            bool isExecuteMethod = false;
+            foreach( string str in cmds )
             {
-                // メソッド呼び出しのサブオプションがtrue
-                if( str.StartsWith("-") )
+                if( isExecuteMethod )
                 {
-                    // 他のサブオプションのため、終了
-                    break;
+                    // メソッド呼び出しのサブオプション
+                    if( str.StartsWith("-") )
+                    {
+                        // 他のサブオプションのため、終了
+                        break;
+                    }
+                    // 引数を追加
+                    args.Add( str );
                 }
-                // 引数を追加
-                args.Add( str );
+                else
+                {
+                    isExecuteMethod = str == EXECUTE_METHOD;
+                }
             }
-            else
+            if( args.Count != 0 )
             {
-                isExecuteMethod = str == EXECUTE_METHOD;
+                args.RemoveAt( 0 );
             }
+            // 返す
+            return args.ToArray();
         }
-        if( args.Count != 0 )
-        {
-            args.RemoveAt( 0 );
-        }
-        // 返す
-        return args.ToArray();
     }
-#if false
-    public static void DebugMethod() 
-    {
-        string[] str = GetMethodParametors();
-        test( str[0], str[1] );
-    }
-
-    public static void test ( string test1, string test2 )
-    {
-        UnityEngine.Debug.Log( test1 + " " + test2 );
-    }
-#endif
 }
